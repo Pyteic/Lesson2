814ef66c702dd90a7c54013291a6150c4b9ee627
Sergey Lyhanin <sergeyluxanin@gmail.com>
2025-05-21 20:53:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+//***1***
+
+/*let a = "1"
+if(a>=0){
+console.log(true);
+}  if(a<0){
+    console.log(false); // true 
+}
+
+let a = "0"
+if(a>=0){
+console.log(true);
+}  if(a<0){
+    console.log(false); //true
+}
+
+let a = "-3"
+if(a>=0){
+console.log(true);
+}  if(a<0){
+    console.log(false); //false
+}*/
+
+//***2***
+
+/*let work = "test"
+if(work ==="test"){
+    console.log(true); //true
+}
+
+let work = "qwerty"
+if(work ==="test"){
+    console.log(true); // -
+}
+let work = "true"
+if(work ==="test"){
+    console.log(true); // -
+}*/
+
+//***3***
+
+/*let a = 1
+if(a>=10){
+    console.log(a-5); 
+} else if(a<10){
+    console.log(a+5); // 6
+}
+
+let a = 10
+if(a>=10){
+    console.log(a-5); 
+} else if(a<10){
+    console.log(a+5); // 5
+}
+
+let a = 3
+if(a>=10){
+    console.log(a-5); 
+} else if(a<10){
+    console.log(a+5); // 8
+}*/
+
+//***4***
+
+/*const  number= prompt("Введіть номер місяця");
+
+let month;
+switch (number) {
+    case "1":
+       month = "Січень";
+       break;
+    case "2":
+       month = "Лютій";
+       break;
+    case "3":
+       month = "Березень";
+       break;
+    case "4":
+       month = "Квітень";
+       break;
+    case "5":
+       month = "Травень";
+       break;
+    case "6":
+       month = "Червень";
+       break;
+    case "7":
+       month = "Липень";
+       break;
+    case "8":
+       month = "Серпень";
+       break;
+    case "9":
+       month = "Вересень";
+       break;
+    case "10":
+       month = "Жовтень";
+       break;
+    case "11":
+       month = "Листопад";
+       break;
+    case "12":
+       month = "Грудень";
+       break;
+
+    default:
+         alert(`Такого місяця${number}, не існує`);   
+}
+
+alert(`${number} місяць ${month}`)*/
+
+//***5***
+
